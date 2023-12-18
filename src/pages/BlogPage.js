6ffdc98f4c70488b5df35c5b7de2b6adb2a6636c
Daniel Montague <danielmontague@DMONTAGUE-MAC.local>
2023-12-18 19:52:34 +0000

    Updated BlogPage component with static content

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,62 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { fetchEntriesByContentType } from '../components/contentfulService';
+import React from 'react';
 
 const BlogPage = () => {
-    const [landingPage, setLandingPage] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const entries = await fetchEntriesByContentType('pageLanding');
-                if (entries.length > 0) {
-                    setLandingPage(entries[0].fields); 
-                } else {
-                    setError('No landing page content found.');
-                }
-            } catch (err) {
-                console.error('Error fetching content:', err);
-                setError('Failed to load content.');
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
-    if (!landingPage) return <div>No landing page content found.</div>;
-
+    // This is a simplified version without fetching data from Contentful
     return (
         <div className="blog-container">
-        <div className="blog-header">
-          <h1 className="blog-title">{landingPage.internalName}</h1>
-          {/* Add SEO Information and other header details */}
+            <div className="blog-header">
+                <h1 className="blog-title">Blog Title</h1>
+                {/* Add SEO Information and other header details */}
+            </div>
+
+            {/* Example Featured Blog Post Section */}
+            <div className="featured-blog-post">
+                <img
+                    src="path-to-your-featured-image.jpg"
+                    alt="Featured Blog Post"
+                />
+                <h2>Featured Blog Post Title</h2>
+                <p>Description of the featured blog post.</p>
+                {/* Render other parts of the blog post as needed */}
+            </div>
+
+            {/* Render additional sections like "Latest articles" */}
         </div>
-    
-        {/* Render Featured Blog Post with Image */}
-        {landingPage.featuredBlogPost && (
-          <div className="featured-blog-post">
-            <img
-              src={landingPage.featuredBlogPost.fields.featuredImage.fields.file.url}
-              alt={landingPage.featuredBlogPost.fields.featuredImage.fields.title || 'Featured Blog Post'}
-            />
-            <h2>{landingPage.featuredBlogPost.fields.title}</h2>
-            <p>{landingPage.featuredBlogPost.fields.description}</p>
-            {/* Render other parts of the blog post as needed */}
-          </div>
-        )}
-    
-        {/* Render additional sections like "Latest articles" */}
-      </div>
-    
     );
 };
 
 export default BlogPage;
+
 
 
 
