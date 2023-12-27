c8b1612ea0664bf80cede8474a0feaa3e6458fd3
Daniel Montague <danielmontague@DMONTAGUE-MAC.local>
2023-12-27 21:23:52 +0000

    ezoic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import WWJDPage from './pages/WWJDPage';
 import BlogPage from './pages/BlogPage';
-import TermsAndConditionsPage from './pages/TermsAndConditionsPage'; // Import your TermsAndConditionsPage component
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage'; // Import your PrivacyPolicyPage component
+import TermsAndConditionsPage from './pages/TermsAndConditionsPage'; // Ensure you have this component created
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage'; // Ensure you have this component created
 
 function App() {
+  useEffect(() => {
+    // Script integration
+    window.ezstandalone = window.ezstandalone || {};
+    window.ezstandalone.cmd = window.ezstandalone.cmd || [];
+    window.ezstandalone.cmd.push(function() {
+      window.ezstandalone.define(188);
+      window.ezstandalone.enable();
+      window.ezstandalone.display();
+    });
+    // The empty dependency array ensures this effect runs once on mount
+  }, []);
+
   return (
     <Router>
       <NavigationBar />
@@ -16,12 +28,13 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/wwjd" element={<WWJDPage />} />
         <Route path="/blog" element={<BlogPage />} />
-        <Route path="/terms-and-conditions" element={<TermsAndConditionsPage />} /> {/* Route to Terms and Conditions */}
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} /> {/* Route to Privacy Policy */}
+        <Route path="/terms-and-conditions" element={<TermsAndConditionsPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
+
 
